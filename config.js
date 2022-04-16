fc640b9c1dceb1ae5df3ea90032d88c05a7dadb2
huk250699 <87457168+huk250699@users.noreply.github.com>
2022-04-16 15:02:34 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Chị Hoa của em ';
+const giftUrl = 'https://www.facebook.com/';
+const eventName = 'Happy Birthday';
+const titleCard = 'Tặng Chị';
+const contentCard = 'Chúc chị sinh nhật vui vẻ, thành công trên con đường đã chọn và nhớ là luôn hạnh phúc và bình an nhé chị yêu của em!';
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
